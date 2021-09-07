f1e4a7ecfbc1bb7e0bdba46e9d9ef522e4111d3b
ankit-khater <ankitkhaterak@gmail.com>
2021-09-07 16:11:10 +0600

    add video stream using peerjs library and and creating view of for video audio add buttons like mute and pause video

--- public/script.js
+++ public/script.js
@@ -1,22 +1,78 @@
+const socket = io("/");
+const chatInputBox = document.getElementById("chat_message");
+const all_messages = document.getElementById("all_messages");
+const main__chat__window = document.getElementById("main__chat__window");
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
-const videoGrid=document.getElementById('video-grid');
-const myVideo = document.createElement('video');
-myVideo.muted=true;
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
 
 let myVideoStream;
-navigator.mediaDevices.getUserMedia({
-    video:true,
-    audio:true
 
-}).then(stream =>{
-    myVideoStream=stream;
-    addVideoStream(myVideo,stream);
-})
+var getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
 
-const addVideoStream=(video,stream) => {
-        video.srcObject=stream;
-        video.addEventListener('loadmetadata', ()=>{
-            video.play();
-        });
-        videoGrid.append(video);
-};
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+
+    document.addEventListener("keydown", (e) => {
+      if (e.which === 13 && chatInputBox.value != "") {
+        socket.emit("message", chatInputBox.value);
+        chatInputBox.value = "";
+      }
+    });
+
+    socket.on("createMessage", (msg) => {
+      console.log(msg);
+      let li = document.createElement("li");
+      li.innerHTML = msg;
+      all_messages.append(li);
+      main__chat__window.scrollTop = main__chat__window.scrollHeight;
+    });
+  });
+
+peer.on("call", function (call) {
+  getUserMedia(
+    { video: true, audio: true },
+    function (stream) {
+      call.answer(stream); // Answer the call with an A/V stream.
+      const video = document.createElement("video");
+      call.on("stream", function (remoteStream) {
+        addVideoStream(video, remoteStream);
+      });
+    },
+    function (err) {
+      console.log("Failed to get local stream", err);
+    }
+  );
+});
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
